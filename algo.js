5d013ad700b096cb9700ab200ec049e0f8242bbe
Johnny Chan <johnnychan2011@gmail.com>
2022-12-07 14:41:40 -0800

    updated

--- algo.js
+++ algo.js
@@ -598,3 +598,16 @@ var fizzBuzz = function(n) {
   }
   return res;
 };
+
+// https://leetcode.com/problems/move-zeroes/
+var moveZeroes = function(nums) {
+  for (let i = nums.length; i >= 0; i--) {
+      if(nums[i] === 0) {
+          nums.push(0);
+          nums.splice(i, 1);
+      }
+  }
+  
+  return nums;
+  
+};
