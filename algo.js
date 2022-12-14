3d3ba086796c60d0ef92e248433b10624be362ae
Johnny Chan <johnnychan2011@gmail.com>
2022-12-13 16:36:45 -0800

    updated

--- algo.js
+++ algo.js
@@ -679,3 +679,54 @@ var generateParenthesis = function(n) {
     return max;
     
 };
+ 
+// https://leetcode.com/problems/longest-palindromic-substring/
+
+var longestPalindrome = function(str) {
+  //edge case for strings with length <= 1;
+if(str.length <= 1) return str;
+
+let longest = "";
+
+for(let i = 0; i < str.length; i++) {
+//check for palindromes of ODD length
+let palindrome = getLongestPal(str, i, i);
+if(palindrome.length > longest.length) longest = palindrome;
+
+//check for palindromes of EVEN length
+palindrome = getLongestPal(str, i, i + 1)
+if(palindrome.length > longest.length) longest = palindrome;
+
+}
+return longest;
+}
+
+//helper function that returns longest pal at given center point.
+function getLongestPal (str, left, right) {
+  while(left >= 0 && right < str.length && str[left] === str[right]) {
+      left--;
+      right++;
+  }
+  return str.slice(left + 1, right)
+}
+
+// https://leetcode.com/problems/sqrtx/solutions/25167/easy-to-understand-javascript-solution/
+var mySqrt = function(x) {
+  let left = 1;
+  let right = Math.floor(x / 2) + 1;
+  let mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else if (mid * mid < x) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return right;
+};
