eef517961926f89669b9bd964e9c36b82c583dfc
Johnny Chan <johnnychan2011@gmail.com>
2022-12-16 15:42:29 -0800

    updated

--- algo.js
+++ algo.js
@@ -774,3 +774,21 @@ s = s.split("");
     }
     return count;
 };
+
+// https://leetcode.com/problems/fizz-buzz/
+var fizzBuzz = function(n) {
+  const res = [];
+  
+  for(let i = 1; i <= n; i++) {
+      if(i % 15 === 0) {
+          res.push(`FizzBuzz`);
+      } else if(i % 3 === 0) {
+          res.push(`Fizz`);
+      } else if(i % 5 === 0) {
+          res.push(`Buzz`);
+      } else {
+          res.push(`${i}`);
+      }
+  }
+  return res;
+};
